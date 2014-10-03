fca21a6688e525d42314847a2d1b3fd51eade835
aquabluefish <aquabluefish@gmail.com>
2014-10-03 22:05:31 +0900

    saveAsPngImage

--- src/hm/orz/bluefish/canvastest/CanvasTest.java
+++ src/hm/orz/bluefish/canvastest/CanvasTest.java
@@ -1,15 +1,24 @@
 package hm.orz.bluefish.canvastest;
 
+import android.app.ActionBar;
+import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CanvasTest extends Activity {
+public class CanvasTest extends Activity
+implements OnNavigationListener {
+
+	TextView tv;
+	CustomView cv;
+	Button bt;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,15 +28,39 @@ public class CanvasTest extends Activity {
 		View layout = inflater.inflate(R.layout.main, null);
 		setContentView(layout);
 
-		TextView tv = (TextView)layout.findViewById(R.id.textView1);
-		final CustomView cv = (CustomView)layout.findViewById(R.id.customView1);
-		Button bt = (Button)layout.findViewById(R.id.button1);
-		
+		tv = (TextView)layout.findViewById(R.id.textView1);
+		cv = (CustomView)layout.findViewById(R.id.customView1);
+		bt = (Button)layout.findViewById(R.id.button1);
+
 		bt.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-	                Toast.makeText(CanvasTest.this, "Click!", Toast.LENGTH_LONG).show();
-	                cv.clearView();
-	        }
-	    });
+				Toast.makeText(CanvasTest.this, "Click!", Toast.LENGTH_LONG).show();
+				cv.saveAsPngImage();
+			}
+		});
+
+		getActionBar().setDisplayShowTitleEnabled(false);
+		getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.color_list, R.layout.spinner);
+		adapter.setDropDownViewResource( R.layout.spinner_down);
+		getActionBar().setListNavigationCallbacks(adapter, this);
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int pos, long id) {
+		Toast.makeText(this, "pos=" + pos, Toast.LENGTH_SHORT).show();
+		int color = Color.WHITE;
+		switch(pos){
+		case 0: color = Color.BLACK; break;
+		case 1: color = Color.RED; break;
+		case 2: color = Color.GREEN; break;
+		case 3: color = Color.BLUE; break;
+		case 4: color = Color.CYAN; break;
+		case 5: color = Color.MAGENTA; break;
+		case 6: color = Color.YELLOW; break;
+		}
+		cv.setColor(color);
+		return true;
 	}
 }
--- src/hm/orz/bluefish/canvastest/CustomView.java
+++ src/hm/orz/bluefish/canvastest/CustomView.java
@@ -1,45 +1,62 @@
 package hm.orz.bluefish.canvastest;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.os.Environment;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class CustomView extends View {
+	private Canvas canvas;
+	private Bitmap bitmap;
 	private Paint  paint;
-	private Path  path;
-
+	private Path   path;
+	private Context appCont;
+	
 	public CustomView(Context cont) {
 		super(cont);
-		initView();
+		initView(cont);
 	}
 
 	public CustomView(Context cont, AttributeSet attr) {
 		super(cont,attr);
-		initView();
+		initView(cont);
 	}
 
-	private void initView() {
+	private void initView(Context cont) {
+		appCont = cont.getApplicationContext();
+		
 		path = new Path();
-
 		paint = new Paint();
-		paint.setColor(0xFF008800);
 		paint.setStyle(Paint.Style.STROKE);
 		paint.setStrokeJoin(Paint.Join.ROUND);
 		paint.setStrokeCap(Paint.Cap.ROUND);
 		paint.setStrokeWidth(10);
 	}
 
-	public void clearView() {
-		invalidate();
+	@Override
+	protected void onDraw(Canvas c){
+		c.drawBitmap(bitmap, 0, 0,null);
+		c.drawPath(path, paint);
 	}
 
 	@Override
-	protected void onDraw(Canvas c){
-		c.drawPath(path, paint);
+	protected void onSizeChanged(int w,int h,int oldw,int oldh) {
+		bitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+		canvas = new Canvas(bitmap);
 	}
 
 	@Override
@@ -50,6 +67,7 @@ public class CustomView extends View {
 
 		switch(event.getAction()){
 		case MotionEvent.ACTION_DOWN:
+			path.reset();
 			path.moveTo(x, y);
 			invalidate();
 			break;
@@ -59,10 +77,56 @@ public class CustomView extends View {
 			break;
 		case MotionEvent.ACTION_UP:
 			path.lineTo(x, y);
+			canvas.drawPath(path, paint);
+			path.reset();
 			invalidate();
 			break;
 		}
 		return true;
+	}
+
+	public void setColor(int color) {
+		paint.setColor(color);
+//		paint.setXfermode(new PorterDuffXfermode(android.graphics.PorterDuff.Mode.CLEAR));
+	}
+
+	public void clearView() {
+		canvas.drawColor(Color.TRANSPARENT, android.graphics.PorterDuff.Mode.CLEAR);
+		invalidate();
+	}
+
+	public void saveAsPngImage(){
+		try {
+			File extStrageDir =
+					Environment.getExternalStorageDirectory();
+			File file = new File(
+				extStrageDir.getAbsolutePath() + "/" + Environment.DIRECTORY_DCIM,
+				getFileName());
+			FileOutputStream outStream = new FileOutputStream(file);
+			bitmap.compress(CompressFormat.PNG, 100, outStream);
+			outStream.close();
+
+			Toast.makeText(appCont,"Image saved",Toast.LENGTH_SHORT).show();
+		} catch (FileNotFoundException e) {
+			Toast.makeText(appCont,"FileNotFoundException!!",Toast.LENGTH_SHORT).show();
+			e.printStackTrace();
+		} catch (IOException e) {
+			Toast.makeText(appCont,"IOException!!",Toast.LENGTH_SHORT).show();
+			e.printStackTrace();
+		}
+	}
+
+	protected String getFileName(){
+		Calendar c = Calendar.getInstance();
+		String s = c.get(Calendar.YEAR)
+				+ "_" + (c.get(Calendar.MONTH)+1)
+				+ "_" + c.get(Calendar.DAY_OF_MONTH)
+				+ "_" + c.get(Calendar.HOUR_OF_DAY)
+				+ "_" + c.get(Calendar.MINUTE)
+				+ "_" + c.get(Calendar.SECOND)
+				+ "_" + c.get(Calendar.MILLISECOND)
+				+ ".png";
+		return s;
 	}
 
 	@Override
